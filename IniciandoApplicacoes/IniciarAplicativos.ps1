452398c09074181e77081a4419fbe202471c0a52
rodrigolsr <rodrigolsr@gmail.com>
2016-06-07 18:49:29 -0300

    Teste

--- IniciandoApplicacoes/IniciarAplicativos.ps1
+++ IniciandoApplicacoes/IniciarAplicativos.ps1
@@ -24,6 +24,8 @@ Write-Host "Aguarde enquanto inicio suas aplicacoes..."
 # https://technet.microsoft.com/en-us/library/hh849848.aspx
 # http://ss64.com/ps/start-process.html
 
+
+###############################################################################################
 # Abrir sistemas mais utilizados :
 
 # Open Microsoft DotNet IDE
@@ -35,24 +37,24 @@ Write-Host "Aguarde enquanto inicio suas aplicacoes..."
 # Open Google Browser
 [Diagnostics.Process]::Start("C:\Program Files (x86)\Google\Chrome\Application\chrome.exe")
 
+# Run default e-mail application
+[Diagnostics.Process]::Start("C:\Program Files\Microsoft Office 15\root\office15\OUTLOOK.EXE")
+
 #start-process myfile.txt -workingdirectory "C:\PS-Test" -verb Print
 
 
+###############################################################################################
 # TODO: Open endereços internos mais utilizados
 # Agora executa um teste no site
 #$ie=New-Object -com internetexplorer.application;
 #$ie.visible=$true;
 #$ie.Navigate("http://localhost:$appPort/");
 
-
-# Run default e-mail application
-[Diagnostics.Process]::Start("C:\Program Files\Microsoft Office 15\root\office15\OUTLOOK.EXE")
-
 # Runs Sublime Teste using the Static Start method and opens a Apol folder
 [Diagnostics.Process]::Start("C:\Program Files\Sublime Text 3\sublime_text.exe","N:\Apol")
 
 # Runs Sublime Teste using the Static Start method and opens a Apol folder
-#[Diagnostics.Process]::Start("C:\Program Files\Sublime Text 2\sublime_text.exe","N:\Webseek")
+[Diagnostics.Process]::Start("C:\Program Files\Sublime Text 3\sublime_text.exe","N:\Webseek")
 
 # TODO: Set Netuno:101 Apol on IE Browser, login and open reconnect.asp
 
@@ -60,7 +62,14 @@ Write-Host "Aguarde enquanto inicio suas aplicacoes..."
 #[Diagnostics.Process]::Start("D:\Ferramentas\Executor64bitTestBuild\Executor.exe")
 
 # Run timesheet on Excel
-[Diagnostics.Process]::Start("C:\Program Files\Microsoft Office 15\root\office15\EXCEL.EXE", "C:\Users\RLessa\Desktop\ponto201601.xlsx")
+#[Diagnostics.Process]::Start("C:\Program Files\Microsoft Office 15\root\office15\EXCEL.EXE", "C:\Users\RLessa\Desktop\ponto201601.xlsx")
+
+# Pastas de trabalho
+$startinfo = new-object System.Diagnostics.ProcessStartInfo 
+$startinfo.FileName = "explorer.exe"
+$startinfo.WorkingDirectory = 'C:\branchSiteLD\Branch_Estatistica_v1.1.0.0'
+
+[System.Diagnostics.Process]::Start($startinfo)
 
 ###############################################################################################
 # Atalhos
@@ -71,6 +80,7 @@ function go-projetos
 {
 	set-location N:\Apol
 }
+
 
 ###############################################################################################
 # SVN - checkout de todos os diretórios das aplicações
@@ -85,10 +95,12 @@ $svnExePath = "C:\Program Files\TortoiseSVN\bin\SVN.exe";
 # Verifica se o SVN client está instalado
 if ((test-path "HKLM:\Software\TortoiseSVN") -eq $false) {
 	Write-Host "";
-	Write-Host -foregroundColor Red "Erro: O Tortoise, Cliente SVN, nÃ£o está instalado.";
+	Write-Host -foregroundColor Red "Erro: O Tortoise, Cliente SVN, nao está instalado.";
 	return;
 }
 
+
+###############################################################################################
 #& $svnExePath checkout $svnBasePath$svnRepository$svnBranch$app $fullPhysicalPath$app;
 
 Write-Host " ";
