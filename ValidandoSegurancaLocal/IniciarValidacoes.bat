9898ce7144a754a8ad7de20043e81e454875d62a
rodrigolsr <rodrigolsr@gmail.com>
2018-06-12 18:44:43 -0300

    Todo list

--- ValidandoSegurancaLocal/IniciarValidacoes.bat
+++ ValidandoSegurancaLocal/IniciarValidacoes.bat
@@ -1,1 +1,11 @@
-PowerShell -NoProfile -ExecutionPolicy Bypass -noexit -Command "& 'D:\github\scripts.launcher.deploy\ValidandoSegurancaLocal\CheckAntivirus.ps1'"
+PowerShell -NoProfile -ExecutionPolicy Bypass -noexit -Command "& 'D:\projetos\scripts.launcher.deploy\ValidandoSegurancaLocal\CheckAntivirus.ps1'"
+
+REM TODO: Validar Atualização do sistema operacional
+
+REM Transforma em json validação do anivirus
+
+REM Enviar validações para API na LDSoft
+
+REM Retornar token de login na VPN
+
+REM PowerShell -NoProfile -ExecutionPolicy Bypass -noexit -Command "& 'D:\github\scripts.launcher.deploy\ValidandoSegurancaLocal\CheckAntivirus.ps1'"
