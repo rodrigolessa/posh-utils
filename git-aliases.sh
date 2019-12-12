04c60b686a3e022ae5b1e6f1d4ad015e6888ab85
Rodrigo Lessa <rodrigolsr@gmail.com>
2019-12-12 17:48:56 -0300

    alias

--- git-aliases.sh
+++ git-aliases.sh
@@ -22,17 +22,19 @@
 
 # n = !git checkout -b
 
+# git diff --name-only master
+
 # git diff --name-only bug/8741..master
 
 # git archive -o pacote.zip HEAD $(git diff --name-only bug/8741..master)
 
 # git log --pretty=format:"%h - %an, %ar : %s" --name-only
 
-#my_alias = "!f() { 〈your complex command〉 }; f"
+# my_alias = "!f() { 〈your complex command〉 }; f"
 
-#reset = !git reset --hard HEAD && git clean -qfdx
+# reset = !git reset --hard HEAD && git clean -qfdx
 
-#ra = "!f() { git remote add $1 https://bitbucket.org/$2.git; }; f"
+# ra = "!f() { git remote add $1 https://bitbucket.org/$2.git; }; f"
 
-#d = "!f() { git diff --name-only @r..master; }; f"
-#d = "!f() { git diff --name-only HEAD^; }; f"
+# d = "!f() { git diff --name-only @r..master; }; f"
+# d = "!f() { git diff --name-only HEAD^; }; f"
---
+++ git-utils.ps1
@@ -0,0 +1,8 @@
+# Install Modules
+
+Import-Module posh-git            # https://github.com/dahlbyk/posh-git
+Import-Module oh-my-posh          # https://github.com/JanDeDobbeleer/oh-my-posh
+Import-Module PsGoogle            # https://github.com/gfody/PsGoogle
+Import-Module PSSudo              # https://github.com/ecsousa/PSSudo
+Import-Module DockerCompletion    # https://github.com/matt9ucci/DockerCompletion
+Import-Module Get-ChildItemColor  # https://github.com/joonro/Get-ChildItemColor
